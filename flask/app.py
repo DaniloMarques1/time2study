fd5ed8ff8ede8bf0d2b08e61d9cef7a3f6523c7a
Danilo Marques <danilomarques20@hotmail.com>
2019-06-09 00:56:40 -0300

    Ajuste no arquivo python para tratar a inserção de um novo usuario(registro)

--- flask/app.py
+++ flask/app.py
@@ -1,15 +1,58 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine("mysql://danilo@localhost/time2study")
+
+db = scoped_session(sessionmaker(bind=engine))
 
 
 app = Flask(__name__)
 
 CORS(app)
 
+@app.route("/")
+def index():
+	data = db.execute("SELECT * FROM User").fetchall()
+	print(data)
+	return "opa"
+
+# @app.route("/registrar", methods=["POST"])
+# def registrar():
+# 	name, email, password = request.form.get("name"), request.form.get("email"), request.form.get("password")
+# 	if name is not None and email is not None and password is not None:
+# 		try:
+# 			print("opa")
+# 			a = db.execute("INSERT INTO User(name, email,password) VALUES(:name, :email, :password)", {"name" : name, "email" : email,"password" : password})
+
+# 			print(a)
+# 		except:
+# 			json = {"success" : False}
+# 		else:
+# 			db.commit()
+# 			json = {"success" : True}	
+	
+# 	return jsonify(json)
+
 @app.route("/registrar", methods=["POST"])
 def registrar():
-	name, email, senha = request.get.form("nome"), request.get.form("email"), request.get.form("senha")
-	print(name, email, senha)
+	'''
+	Recebera os dados enviados via post do front end e tentara inserir ao banco de dados
+	'''
+	name, email, password = request.form.get("name"), request.form.get("email"), request.form.get("password")
+	result = db.execute("SELECT * FROM User WHERE email = :email", {"email" : email}).fetchone()
+	if not result:
+		a = db.execute("INSERT INTO User(name, email,password) VALUES(:name, :email, :password)", {"name" : name, "email" : email,"password" : password})
+		db.commit()
+		json = {"success" : True}
+	else:
+		json  = {"success" : False}	
+	
+	return jsonify(json)
+
+
+
 if __name__ == "__main__":
 	app.run(debug=True)
 
