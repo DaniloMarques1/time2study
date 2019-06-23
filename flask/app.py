4258d1c60fabc7ed417ca5df1d6a069ed60ef01f
Danilo Marques <danilomarques20@hotmail.com>
2019-06-23 01:01:34 -0300

    Requirements da aplicação

--- flask/app.py
+++ flask/app.py
@@ -3,14 +3,11 @@ from flask_cors import CORS
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-
-
 engine = create_engine("mysql://danilo@localhost/time2study")
 
 db = scoped_session(sessionmaker(bind=engine))
 
 app = Flask(__name__)
-
 
 CORS(app)
 
