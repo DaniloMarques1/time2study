2c5bd7d79f49f5d29197b27b6afea810a4e2c3d7
Danilo Marques <danilomarques20@hotmail.com>
2019-07-10 20:41:38 -0300

    Ajuste na criação da(s) tabela(s) do sistema

--- data.sql
+++ data.sql
@@ -3,10 +3,20 @@ CREATE DATABASE time2study;
 use time2study;
 
 CREATE TABLE User(
-	id int AUTO_INCREMENT PRIMARY KEY,
+	id_user int AUTO_INCREMENT PRIMARY KEY,
 	name varchar(50) NOT NULL,
 	email varchar(50) UNIQUE NOT NULL,
 	password varchar(20) NOT NULL
+);
+
+CREATE TABLE Task(
+	id_task int AUTO_INCREMENT PRIMARY KEY,
+    id_user int not null,
+	title varchar(40) NOT NULL,
+	qtd_pomodoros int NOT NULL,
+	description varchar(400),
+	finished ENUM('1', '0') DEFAULT '0',
+	FOREIGN KEY(id_user) REFERENCES User(id_user)
 );
 
 
