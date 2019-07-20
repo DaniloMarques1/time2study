99ef8fef9d0c1576076e385c32e8229f95fc8582
Danilo Marques <danilomarques20@hotmail.com>
2019-07-20 09:29:22 -0300

    Alterando o controle de acesso a home da pagina, utilizando o endpoint para verificar um token valido

--- js/control_access.js
+++ js/control_access.js
@@ -1,9 +1,16 @@
-//Função que checa o local storage para ve se o usuario esta logado
-const isLog = () => {
-    const flag = localStorage.getItem("log")
-    if (flag == "false") {
-        window.location.href = "logar.html"
-    }
+//Só permite ir para a pagina de login caso não esteja logado
+const isLogged = () => {
+	const token = localStorage.getItem("token")
+	var myHeaders = new Headers()
+	myHeaders.append("Authorization", "JWT " + token)
+	myHeaders.append("Content-Type", "application/json")
+	fetch("http://localhost:5000/user", {headers: myHeaders})
+	.then(response => {
+		if (!response.ok) {
+			window.location.href = "logar.html"
+		}
+	});
+	
 }
 
-isLog()
+isLogged()
